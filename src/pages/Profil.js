359fb3df848b347981c046973bc752d467c377d9
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-10-04 16:44:39 +0200

    all the && for icons done

--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { usersContext } from "../contexts/Users";
 
 const Profil = () => {
+  const [hover, setHover] = useState("user");
   const { user } = useContext(usersContext);
-  const handleMouseEnter = (e) => {
-    // console.log(e.target.id);
+  const handleMouseEnter = (hover) => {
+    setHover(hover);
   };
   return (
     <main>
@@ -14,14 +15,52 @@ const Profil = () => {
             <>
               <article className="cardContainer">
                 <img src={oneUser.picture.medium} alt="" />
-                <p>Hi, my name is</p>
-                <p>
-                  {oneUser.name.title}
-                  {`  `}
-                  {oneUser.name.first}
-                  {`  `}
-                  {oneUser.name.last}
-                </p>
+                {hover === "user" && (
+                  <>
+                    <p>Hi, my name is</p>
+                    <p>
+                      {oneUser.name.title}
+                      {`  `}
+                      {oneUser.name.first}
+                      {`  `}
+                      {oneUser.name.last}
+                    </p>
+                  </>
+                )}
+
+                {hover === "envelope" && (
+                  <>
+                    <p>My email adress is</p>
+                    <p>{oneUser.email}</p>
+                  </>
+                )}
+                {hover === "calendar" && (
+                  <>
+                    <p>My birthday is</p>
+                    <p>{oneUser.dob.date}</p>
+                  </>
+                )}
+                {hover === "location" && (
+                  <>
+                    <p>My adress is</p>
+                    <p>
+                      {oneUser.location.street.name}{" "}
+                      {oneUser.location.street.number}
+                    </p>
+                  </>
+                )}
+                {hover === "phone" && (
+                  <>
+                    <p>My phone number is</p>
+                    <p>{oneUser.cell}</p>
+                  </>
+                )}
+                {hover === "lock" && (
+                  <>
+                    <p>My password is</p>
+                    <p>{oneUser.login.password}</p>
+                  </>
+                )}
               </article>
               <article className="icon">
                 <div className="innerIcon">
@@ -40,7 +79,7 @@ const Profil = () => {
                   <span onMouseEnter={() => handleMouseEnter("phone")}>
                     <i class="fa-solid fa-phone"></i>
                   </span>
-                  <span id="testID" onMouseEnter={handleMouseEnter}>
+                  <span onMouseEnter={() => handleMouseEnter("lock")}>
                     <i class="fa-solid fa-lock"></i>
                   </span>
                 </div>
